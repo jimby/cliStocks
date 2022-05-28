4096c63cbd1832104312bf4bb4a481aed50f4835
jimby <jandora.knox@gmail.com>
2022-05-27 21:04:38 -0700

    new files MainMenu.sh, EditMenu.sh, etc

---
+++ MainMenu.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Bash Menu Script Example
+PS3="please enter your choice"
+options=("Edit menu" "Add menu" "Find menu" "Report menu" "Quit")
+echo "Main Menu"
+select opt in "${options[@]}"
+do
+
+    case $opt in
+        "Edit menu")
+            echo "open EditMenu.sh"
+            # python ./UpdatePrices.py
+            ./EditMenu.sh
+            break
+            ;;
+        "Add menu")
+            echo "Insert Menu"
+            break
+            ;;
+
+        "Find menu")
+            echo "Find Menu"
+	        # ./find_menu.sh
+	        break
+            ;;
+        "Report menu")
+            echo "Report Menu"
+	        # ./find_menu.sh
+	        break
+            ;;
+        "Quit")
+            exit 0
+            ;;
+
+        *) echo "invalid option $REPLY";;
+    esac
+
+done
+
